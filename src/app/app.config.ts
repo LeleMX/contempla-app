28f696fadc8c9115497fd1be388ce33d0d8ba45e
IA Contempla <ia@contempla.com>
2026-02-22 22:28:12 -0700

    chore: move firebase config to ignored environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,26 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDe6p2LXGtA3GewuO5dVTY0TMRgbyy5yqM",
-  authDomain: "contempla-app.firebaseapp.com",
-  projectId: "contempla-app",
-  storageBucket: "contempla-app.firebasestorage.app",
-  messagingSenderId: "444753192597",
-  appId: "1:444753192597:web:9158739bdaea195bf79d9c",
-  measurementId: "G-NFXKHBWR6T"
-};
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
     provideCharts(withDefaultRegisterables()),
